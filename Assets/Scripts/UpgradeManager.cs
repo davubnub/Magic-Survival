279e9f939ea9ff3a2cf8f95c8b72d2828ecd03ed
LemonFizz2814 <blake2704@outlook.com>
2022-06-15 12:17:41 +1200

    Update

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -15,32 +15,30 @@ public class UpgradeManager : MonoBehaviour
 
     const int amountOfOptions = 3;
 
-    string[] upgradeNames = { "Bigger penis", "Chonky", "Thicc ass", "Become gay" };
-    string[] upgradeDescriptions = { "Penis becomes very long yes", "chonk", "super thicc", "gay" };
-
-    private void Start()
-    {
-        ShowUpgradeUI(false);
-    }
+    string[] upgradeNames           = { "Bigger benis", "Chonky", "Thicc ass", "Become gay" };
+    string[] upgradeDescriptions    = { "very long yes", "chonk", "this is a description", "gay" };
 
     public void ShowUpgradeUI(bool _active)
     {
+        print("active");
         gameObject.SetActive(_active);
     }
 
     public void SelectOptions()
     {
-        int[] options = { 0, 0, 0 };
-
-        int[] listOfUpgrades = {0, 1, 2, 3};
+        int[] options        = { 0, 0, 0, 0 };
+        int[] listOfUpgrades = { 0, 1, 2, 3 };
 
         for(int i = 0; i < amountOfOptions; i++)
         {
+            print("listOfUpgrades " + listOfUpgrades.Length);
             //pick random upgrade
-            options[i] = Random.Range(0, listOfUpgrades.Length);
+            int num = listOfUpgrades[Random.Range(0, listOfUpgrades.Length)];
             //remove that upgrade from the list of upgrades to choose from
-            listOfUpgrades[System.Array.IndexOf(listOfUpgrades, options[i])] = listOfUpgrades[listOfUpgrades.Length - 1];
+            listOfUpgrades[System.Array.IndexOf(listOfUpgrades, num)] = listOfUpgrades[listOfUpgrades.Length - 1];
             System.Array.Resize(ref listOfUpgrades, listOfUpgrades.Length - 1);
+
+            options[i] = num;
 
             upgradeNameText[i].text = upgradeNames[options[i]];
             upgradeDescriptionText[i].text = upgradeDescriptions[options[i]];
