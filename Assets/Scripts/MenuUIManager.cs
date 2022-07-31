a5358fa81137a1f14cc3d1b8f6f7519c1315045e
LemonFizz2814 <blake2704@outlook.com>
2022-07-31 15:13:43 +1200

    Ads

--- Assets/Scripts/MenuUIManager.cs
+++ Assets/Scripts/MenuUIManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
+using UnityEngine.Purchasing;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class MenuUIManager : MonoBehaviour
+public class MenuUIManager : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
 {
     public TextMeshProUGUI scoreText; 
     public TextMeshProUGUI highScoreText;
@@ -15,6 +17,7 @@ public class MenuUIManager : MonoBehaviour
     public CoinSpawner coinSpawner;
     public GameObject prizePrompt;
     public GameObject purchasePrompt;
+    public GameObject watchAdPrompt;
     public PurchaseScript purchaseScript;
     public PrizeScript prizeScript;
     public PoolingManager poolingManager;
@@ -27,6 +30,52 @@ public class MenuUIManager : MonoBehaviour
     public bool hasSound = true;
     public GameObject MusicButton;
     public GameObject SoundButton;
+
+    [Header("Ad Settings")]
+    public string _androidGameId;
+    public string _iOSGameId;
+    public bool _testMode = true;
+    public int coinsFromWatchingAd;
+    public int chanceForAd;
+    public int chanceToShowWatchAd;
+
+    string kRemoveAds = "removeads";
+    static IStoreController m_StoreController;
+    static IExtensionProvider m_StoreExtensionProvider;
+    string _gameId;
+    string _adUnitId;
+    bool isIOS;
+
+    enum EADTYPE
+    {
+        nothing,
+        coin,
+    }
+
+    EADTYPE adType;
+
+    void Awake()
+    {
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {
+        isIOS = Application.platform == RuntimePlatform.IPhonePlayer;
+        _gameId = (isIOS)
+            ? _iOSGameId
+            : _androidGameId;
+        Advertisement.Initialize(_gameId, _testMode);
+    }
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
 
     public void RestartPressed()
     {
@@ -86,11 +135,123 @@ public class MenuUIManager : MonoBehaviour
         PlayerPrefs.SetInt("Day", System.DateTime.Today.Day);
         UpdateCoinTexts();
     }
+    public void ShowAd()
+    {
+        _adUnitId = (isIOS) ? "Interstitial_iOS" : "Interstitial_Android";
+        adType = EADTYPE.nothing;
+        Advertisement.Show(_adUnitId, this);
+    }
+    public void WatchAd()
+    {
+        _adUnitId = (isIOS) ? "Rewarded_iOS" : "Rewarded_Android";
+        Debug.Log("Showing Ad: " + _adUnitId);
+        adType = EADTYPE.coin;
+        Advertisement.Show(_adUnitId, this);
+    }
+    public void ShowBanner()
+    {
+        Advertisement.Banner.Show("bannerPlacement");
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+    }
+
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        // Optionally execute code if the Ad Unit successfully loads content.
+
+        switch (adType)
+        {
+            case EADTYPE.coin:
+                Debug.Log($"LOADED");
+                PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + coinsFromWatchingAd);
+                break;
+        }
+    }
+
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit: {adUnitId} - {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to load, such as attempting to try again.
+    }
+
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to show, such as loading another ad.
+    }
+
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { }
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState) { }
+
+    public void InitializePurchasing()
+    {
+        // TODO: uncomment and fix
+        /*if (IsInitialized())
+        {
+            return;
+        }
+        // Collects products and store-specific configuration details
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        // Add a product with a Unity IAP ID, type
+        builder.AddProduct(kRemoveAds, ProductType.Consumable);
+        //Initialize Unity IAP with the specified listener and configuration
+        // Store Controller and Extension provider are set.
+        UnityPurchasing.Initialize(this, builder);*/
+    }
+    public void BuyRemoveAds(string productId)
+    {
+        if (IsInitialized())
+        {
+            Product product = m_StoreController.products.WithID(productId);
+            if (product != null && product.availableToPurchase)
+            {
+                Debug.Log("Purchasing Product");
+                m_StoreController.InitiatePurchase(product);
+            }
+            else
+            {
+                Debug.Log("Product either is not found or not available for purchase");
+            }
+        }
+        else
+        {
+            Debug.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        if (Equals(args.purchasedProduct.definition.id, kRemoveAds))
+        {
+            Debug.Log("Purchasing Product");
+            PlayerPrefs.SetInt("boughtAds", 1);
+        }
+        else
+        {
+            Debug.Log("Unrecognized Product");
+        }
+        return PurchaseProcessingResult.Complete;
+    }
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: { 1}", product.definition.storeSpecificId, failureReason));
+    }
+    private bool IsInitialized()
+    {
+        // Check if both the Purchasing references are set
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
     public void GameOver(int _coins)
     {
         purchasePrompt.SetActive(_coins >= coinsToPurchase);
+        watchAdPrompt.SetActive(Random.Range(0, 100) < chanceToShowWatchAd);
         prizePrompt.SetActive(PlayerPrefs.GetInt("Day", 0) != System.DateTime.Today.Day);
         UpdateCoinTexts();
+
+        if(Random.Range(0, 100) < chanceForAd)
+        {
+            ShowAd();
+        }
     }
 
     void UpdateCoinTexts()
