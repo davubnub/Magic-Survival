b6bfaf66cf77409f993d4c188410a49cae888921
StarPatric <unigeorge98@gmail.com>
2023-02-02 00:15:56 +1300

    More Math Function

---
+++ Assets/Scripts/World Generation Scripts/ProceduralObjectGenerator1.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralObjectGenerator1 : MonoBehaviour
+{
+    [Header("Debugging Purpose Only")]
+    public int Seed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random.InitState(Seed);
+
+        List<List<float>> points = random_locations(5, 2, 100.0f);
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            for (int j = 0; j < points[i].Count; j++)
+            {
+                print(points[i][j]);
+            }
+        }
+    }
+
+    float random_gaussian()
+    {
+        float u, v, S;
+
+        do
+        {
+            u = 2.0f * Random.Range(0.0f, 1.0f) - 1.0f;
+            v = 2.0f * Random.Range(0.0f, 1.0f) - 1.0f;
+            S = u * u + v * v;
+        }
+        while (S >= 1.0);
+
+        float fac = Mathf.Sqrt(-2.0f * Mathf.Log(S) / S);
+
+        return u * fac;
+    }
+
+    List<List<float>> random_directions(int _num_points, int _dimension)
+    {
+        List<List<float>> random_directions = new List<List<float>>();
+
+        for (int i = 0; i < _dimension; i++)
+        {
+            List<float> dimension = new List<float>();
+
+            for (int j = 0; j < _num_points; j++)
+            {
+                dimension.Add(random_gaussian());
+            }
+
+            random_directions.Add(dimension);
+        }
+
+        return random_directions;
+    }
+
+    float frobeniusNorm(List<List<float>> matrix)
+    {
+        // To store the sum of squares of the
+        // elements of the given matrix
+        float sumSq = 0;
+        for (int i = 0; i < matrix.Count; i++)
+        {
+            for (int j = 0; j < matrix[i].Count; j++)
+            {
+                sumSq += Mathf.Pow(matrix[i][j], 2.0f);
+            }
+        }
+
+        // Return the square root of
+        // the sum of squares
+        float res = Mathf.Sqrt(sumSq);
+
+        return res;
+    }
+
+    List<List<float>> apply_frobeniusNorm_to_random_directions(List<List<float>> random_directions)
+    {
+        float frobeniusNorm_value = frobeniusNorm(random_directions);
+
+        List<List<float>> frobeniusNorm_applied = new List<List<float>>();
+
+        for (int i = 0; i < random_directions.Count; i++)
+        {
+            List<float> dimension = new List<float>();
+
+            for (int j = 0; j < random_directions[i].Count; j++)
+            {
+                float applied = random_directions[i][j] / frobeniusNorm_value;
+
+                dimension.Add(applied);
+            }
+
+            frobeniusNorm_applied.Add(dimension);
+        }
+
+        return random_directions;
+    }
+
+    List<List<float>> points_in_given_radius(List<List<float>> random_directions, int num_points, float dimension)
+    {
+        float random_radii = Mathf.Pow(Random.Range(0, num_points + 1), (1 / dimension));
+
+        for (int i = 0; i < random_directions.Count; i++)
+        {
+            for (int j = 0; j < random_directions[i].Count; j++)
+            {
+                random_directions[i][j] *= random_radii;
+            }
+        }
+
+        return random_directions;
+    }
+
+    List<List<float>> transpose_points(List<List<float>> points)
+    {
+        List<List<float>> results = new List<List<float>>();
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            for (int j = 0; j < points[i].Count; j++)
+            {
+                points[i][j] = points[j][i];
+            }
+        }
+
+        return results;
+    }
+
+    List<List<float>> points_on_radius(List<List<float>> points, float radius)
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            for (int j = 0; j < points[i].Count; j++)
+            {
+                points[i][j] *= radius;
+            }
+        }
+
+        return points;
+    }
+
+    List<List<float>> random_locations(int _num_points, int _dimension, float _radius)
+    {
+        List<List<float>> random_directions_list = random_directions(_num_points, _dimension);
+        List<List<float>> random_directions_normalized = apply_frobeniusNorm_to_random_directions(random_directions_list);
+
+        List<List<float>> random_directions_radius = points_in_given_radius(random_directions_normalized, _num_points, _dimension);
+        List<List<float>> random_directions_transposed = transpose_points(random_directions_radius);
+        List<List<float>> final_points = points_on_radius(random_directions_transposed, _radius);
+
+        return final_points;
+    }
+}
+
+//# Generate "num_points" random points in "dimension" that have uniform
+//# probability over the unit ball scaled by "radius" (length of points
+//# are in range [0, "radius"]).
+
+//def random_ball(num_points, dimension, radius= 1):
+
+//    from numpy import random, linalg
+
+//# First generate random directions by normalizing the length of a
+//# vector of random-normal values (these distribute evenly on ball).
+
+//    random_directions = random.normal(size = (dimension, num_points))
+
+//    random_directions /= linalg.norm(random_directions, axis = 0)
+
+//    # Second generate a random radius with probability proportional to
+//    # the surface area of a ball with a given radius.
+//    random_radii = random.random(num_points) * *(1 / dimension)
+//    # Return the list of random (direction & length) points.
+//    return radius * (random_directions * random_radii).T
