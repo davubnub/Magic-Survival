ea623773ad3e3d15778d918605cc60c61bf34aa8
StarPatric <unigeorge98@gmail.com>
2023-02-03 00:50:25 +1300

    It generates deterministically
    need a better formula to generate seed

--- Assets/Scripts/World Generation Scripts/ProceduralObjectGenerator1.cs
+++ Assets/Scripts/World Generation Scripts/ProceduralObjectGenerator1.cs
@@ -9,189 +9,76 @@ public class ProceduralObjectGenerator1 : MonoBehaviour
     public float radius;
 
     [Header("Debugging Purpose Only")]
-    public int Seed = 0;
+    public int Seed;
 
-    // Start is called before the first frame update
-    void Start()
+    Vector3 pointzero = new Vector3();
+    List<Vector3> playerspawn = new List<Vector3>();
+
+    private void Update()
     {
-        Random.InitState(Seed);
-
-        List<Vector2> spawnlocations = Convert_float_to_Vector(num_points, radius);
-
-        for (int i = 0; i < spawnlocations.Count; i++)
+        //Is the player x distance away from old position
+        if (Vector3.Distance(pointzero, this.transform.position) > 100)
         {
-            GameObject obj = GameObject.Instantiate(ObjecttoSpawn, new Vector3(spawnlocations[i].x, this.transform.position.y, spawnlocations[i].y), Quaternion.identity);
-        }
-    }
+            //Current point
+            pointzero = this.transform.position;
 
-    float random_gaussian()
-    {
-        float u, v, S;
-
-        do
-        {
-            u = 2.0f * Random.Range(0.0f, 1.0f) - 1.0f;
-            v = 2.0f * Random.Range(0.0f, 1.0f) - 1.0f;
-            S = u * u + v * v;
-        }
-        while (S >= 1.0);
-
-        float fac = Mathf.Sqrt(-2.0f * Mathf.Log(S) / S);
-
-        return u * fac;
-    }
-
-    List<List<float>> random_directions(int _num_points, int _dimension)
-    {
-        List<List<float>> random_directions = new List<List<float>>();
-
-        for (int i = 0; i < _dimension; i++)
-        {
-            List<float> dimension = new List<float>();
-
-            for (int j = 0; j < _num_points; j++)
+            //To find point closet to player
+            float leastdistancetoplayer = float.MaxValue;
+            Vector3 closestpointtoplayer = new Vector3();
+            for (int i = 0; i < playerspawn.Count; i++)
             {
-                dimension.Add(random_gaussian());
-            }
-
-            random_directions.Add(dimension);
-        }
-
-        return random_directions;
-    }
-
-    float frobeniusNorm(List<List<float>> matrix)
-    {
-        // To store the sum of squares of the
-        // elements of the given matrix
-        float sumSq = 0;
-        for (int i = 0; i < matrix.Count; i++)
-        {
-            for (int j = 0; j < matrix[i].Count; j++)
-            {
-                sumSq += Mathf.Pow(matrix[i][j], 2.0f);
-            }
-        }
-
-        // Return the square root of
-        // the sum of squares
-        float res = Mathf.Sqrt(sumSq);
-
-        return res;
-    }
-
-    List<List<float>> apply_frobeniusNorm_to_random_directions(List<List<float>> random_directions)
-    {
-        float frobeniusNorm_value = frobeniusNorm(random_directions);
-
-        List<List<float>> frobeniusNorm_applied = new List<List<float>>();
-
-        for (int i = 0; i < random_directions.Count; i++)
-        {
-            List<float> dimension = new List<float>();
-
-            for (int j = 0; j < random_directions[i].Count; j++)
-            {
-                float applied = random_directions[i][j] / frobeniusNorm_value;
-
-                dimension.Add(applied);
-            }
-
-            frobeniusNorm_applied.Add(dimension);
-        }
-
-        return random_directions;
-    }
-
-    List<List<float>> points_in_given_radius(List<List<float>> random_directions, int num_points, float dimension)
-    {
-        float random_radii = Mathf.Pow(Random.Range(0, num_points + 1), (1 / dimension));
-
-        for (int i = 0; i < random_directions.Count; i++)
-        {
-            for (int j = 0; j < random_directions[i].Count; j++)
-            {
-                random_directions[i][j] *= random_radii;
-            }
-        }
-
-        return random_directions;
-    }
-
-    List<List<float>> transpose_points(List<List<float>> points)
-    {
-        float longest;
-
-        if (points.Count != 0)
-        {
-            float max = 0;
-
-            for (int i = 0; i < points.Count; i++)
-            {
-                for (int j = 0; j < points[i].Count; j++)
+                if (Vector3.Distance(pointzero, playerspawn[i]) < leastdistancetoplayer)
                 {
-                    if (max < points[i][j])
-                    {
-                        max = points[i][j];
-                    }
+                    leastdistancetoplayer = Vector3.Distance(pointzero, playerspawn[i]);
+                    closestpointtoplayer = playerspawn[i];
                 }
             }
 
-            longest = max;
-        }
-        else
-        {
-            longest = 0;
-        }
-
-        List<List<float>> outer = new List<List<float>>();
-        for (int i = 0; i < longest; i++)
-            outer.Add(new List<float>(points.Count));
-        for (int j = 0; j < points.Count; j++)
-            for (int i = 0; i < longest; i++)
-                outer[i].Add(points[j].Count > i ? points[j][i] : default(float));
-        return outer;
-    }
-
-    List<List<float>> points_on_radius(List<List<float>> points, float radius)
-    {
-        for (int i = 0; i < points.Count; i++)
-        {
-            for (int j = 0; j < points[i].Count; j++)
+            //Now that we have the point closest to the player
+            //Is that point greater than x
+            //If it is we are going to spawn new objects
+            //Otherwise we will spawn objects at old locations
+            if (leastdistancetoplayer > 100)
             {
-                points[i][j] *= radius;
+                playerspawn.Add(pointzero);
+
+                float Xmin = pointzero.x - radius;
+                float Xmax = pointzero.x + radius;
+
+                float Ymin = pointzero.z - radius;
+                float Ymax = pointzero.z + radius;
+
+                //Just a linear Function
+                int customseed = (int)((Seed * (Xmin + Xmax)) + (Ymin + Ymax));
+                print(customseed + " = " + Seed + " * " + (Xmin + Xmax) + " + " + (Ymin + Ymax));
+                System.Random _rnd = new System.Random(customseed);
+
+                for (int i = 0; i < num_points; i++)
+                {
+                    Vector3 loc = new Vector3(_rnd.Next((int)Xmin, (int)Xmax), this.transform.position.y, _rnd.Next((int)Ymin, (int)Ymax));
+
+                    GameObject obj = GameObject.Instantiate(ObjecttoSpawn, loc, Quaternion.identity);
+                }
+            }
+            else
+            {
+                float Xmin = closestpointtoplayer.x - radius;
+                float Xmax = closestpointtoplayer.x + radius;
+
+                float Ymin = closestpointtoplayer.z - radius;
+                float Ymax = closestpointtoplayer.z + radius;
+
+                //Just a linear Function
+                int customseed = (int)((Seed * (Xmin + Xmax)) + (Ymin + Ymax));
+                System.Random _rnd = new System.Random(customseed);
+
+                for (int i = 0; i < num_points; i++)
+                {
+                    Vector3 loc = new Vector3(_rnd.Next((int)Xmin, (int)Xmax), this.transform.position.y, _rnd.Next((int)Ymin, (int)Ymax));
+
+                    GameObject obj = GameObject.Instantiate(ObjecttoSpawn, loc, Quaternion.identity);
+                }
             }
         }
-
-        return points;
-    }
-
-    List<List<float>> random_locations(int _num_points, int _dimension, float _radius)
-    {
-        List<List<float>> random_directions_list = random_directions(_num_points, _dimension);
-        List<List<float>> random_directions_normalized = apply_frobeniusNorm_to_random_directions(random_directions_list);
-
-        List<List<float>> random_directions_radius = points_in_given_radius(random_directions_normalized, _num_points, _dimension);
-        List<List<float>> random_directions_transposed = transpose_points(random_directions_radius);
-        List<List<float>> final_points = points_on_radius(random_directions_transposed, _radius);
-
-        return final_points;
-    }
-
-    List<Vector2> Convert_float_to_Vector(int num_points, float radius)
-    {
-        // 2 because the game is 2-dimensional (I may need to rework the math if we change the no. dimensions)
-        List<List<float>> points = random_locations(num_points, 2, radius);
-
-        List<Vector2> spawnpoints = new List<Vector2>();
-
-        for (int i = 0; i < points.Count; i++)
-        {
-            Vector2 spawnlocation = new Vector2(points[i][0], points[i][1]);
-
-            spawnpoints.Add(spawnlocation);
-        }
-
-        return spawnpoints;
     }
 }
