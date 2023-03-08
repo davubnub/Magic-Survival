461e6691a5f806d5036317684998e1daedbc0fa7
StarPatric <unigeorge98@gmail.com>
2023-03-08 21:13:07 +1300

    Basic Terrain Changing

--- Assets/Scripts/World Generation Scripts/WorldChunker.cs
+++ Assets/Scripts/World Generation Scripts/WorldChunker.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class WorldChunker : MonoBehaviour
 {
     private const int MaxNumberChunks = 9;  //For Each Chunk Around the Center Chunk
-    private const float OverlapSphereRadius = 10.0f;    //Completely Arbitrary but seems to work
-
-    private const int BorderlayerId = 6;
-    private const int BorderlayerMask = 1 << BorderlayerId;
 
     private GameObject CurrentChunk;
     private float ChunkXSize;
     private float ChunkZSize;
 
+    private Vector3 InitPos;
+
+    [Header("Tiles")]
     public List<GameObject> WorldChunks = new List<GameObject>();
+
+    [Header("Tile Materials")]
+    public List<Material> WorldMaterials = new List<Material>();
+
+    [Header("Map Generation Values")]
+    public Vector3 TileChangeDistance = new Vector3(100.0f, 0.0f, 100.0f);
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +31,8 @@ public class WorldChunker : MonoBehaviour
         ChunkXSize = WorldChunks[3].transform.position.x;
         ChunkZSize = WorldChunks[3].transform.position.z;
 
+        //Set the Initial Position
+        InitPos = this.transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -50,6 +57,11 @@ public class WorldChunker : MonoBehaviour
                     WorldChunks[i].transform.position = WorldChunks[0].transform.position;
                     WorldChunks[0].transform.position = TempPos.position;
 
+                    //Set to 0 Directional Material
+                    Material new_material = Instantiate(WorldMaterials[Random.Range(0, WorldMaterials.Count)]);
+
+                    WorldChunks[0].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+
                     CurrentChunk = WorldChunks[0];
 
                     break;
@@ -59,9 +71,142 @@ public class WorldChunker : MonoBehaviour
             for (int i = 0; i < MaxNumberChunks; i++)
             {
                 ChunkPositioner(i);
-            }
 
-            //isRechunking = true;
+                TileChanger(WorldChunks[i]);
+            }
+        }
+    }
+
+    int WorldTilePicker(Vector3 _tilepos)
+    {
+        int MaterialIndex = 0;
+
+        //Beyond Material 2
+        if (Vector3.Distance(InitPos, _tilepos) > Vector3.Distance(InitPos, (InitPos + (2 * TileChangeDistance))))
+        {
+            MaterialIndex = 1;
+        }
+
+        //Material 2
+        if (Vector3.Distance(InitPos, _tilepos) < Vector3.Distance(InitPos, (InitPos + (2 * TileChangeDistance))) && Vector3.Distance(InitPos, _tilepos) > Vector3.Distance(InitPos, (InitPos + TileChangeDistance)))
+        {
+            MaterialIndex = 1;
+        }
+
+        //Material 1
+        if (Vector3.Distance(InitPos, _tilepos) < Vector3.Distance(InitPos, (InitPos + TileChangeDistance)))
+        {
+            MaterialIndex = 0;
+        }
+
+        return MaterialIndex;
+    }
+
+    void TileChanger(GameObject _tile)
+    {
+        Material new_material = Instantiate(WorldMaterials[WorldTilePicker(_tile.transform.position)]);
+
+        _tile.transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+    }
+
+    void TileRotator(int _i, float _Xpos, float _Zpos)
+    {
+        float oldperlin = Mathf.PerlinNoise(WorldChunks[_i].transform.position.x, WorldChunks[_i].transform.position.z);
+
+        if (_i == 1)
+        {
+            //Top Left
+            if (Mathf.Abs(oldperlin - Mathf.PerlinNoise(_Xpos, _Zpos)) <= 0.1f)
+            {
+                Material new_material = Instantiate(WorldMaterials[0]);
+
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material.SetFloat("Rotation", 135.0f);
+            }
+        }
+
+        //Top
+        if (_i == 2)
+        {
+            if (Mathf.Abs(oldperlin - Mathf.PerlinNoise(_Xpos, _Zpos)) <= 0.1f)
+            {
+                Material new_material = Instantiate(WorldMaterials[0]);
+
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material.SetFloat("Rotation", 90.0f);
+            }
+        }
+
+        if (_i == 3)
+        {
+            //Top Right
+            if (Mathf.Abs(oldperlin - Mathf.PerlinNoise(_Xpos, _Zpos)) <= 0.1f)
+            {
+                Material new_material = Instantiate(WorldMaterials[0]);
+
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material.SetFloat("Rotation", 45.0f);
+            }
+        }
+
+        if (_i == 4)
+        {
+            //Left
+            if (Mathf.Abs(oldperlin - Mathf.PerlinNoise(_Xpos, _Zpos)) <= 0.1f)
+            {
+                Material new_material = Instantiate(WorldMaterials[0]);
+
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material.SetFloat("Rotation", 180.0f);
+            }
+        }
+
+        if (_i == 5)
+        {
+            //Right
+            if (Mathf.Abs(oldperlin - Mathf.PerlinNoise(_Xpos, _Zpos)) <= 0.1f)
+            {
+                Material new_material = Instantiate(WorldMaterials[0]);
+
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material.SetFloat("Rotation", 360.0f);
+            }
+        }
+
+        if (_i == 6)
+        {
+            //Bottom Left
+            if (Mathf.Abs(oldperlin - Mathf.PerlinNoise(_Xpos, _Zpos)) <= 0.1f)
+            {
+                Material new_material = Instantiate(WorldMaterials[0]);
+
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material.SetFloat("Rotation", -135.0f);
+            }
+        }
+
+        //Bottom
+        if (_i == 7)
+        {
+            if (Mathf.Abs(oldperlin - Mathf.PerlinNoise(_Xpos, _Zpos)) <= 0.1f)
+            {
+                Material new_material = Instantiate(WorldMaterials[0]);
+
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material.SetFloat("Rotation", -90.0f);
+            }
+        }
+
+        if (_i == 8)
+        {
+            //Bottom Right
+            if (Mathf.Abs(oldperlin - Mathf.PerlinNoise(_Xpos, _Zpos)) <= 0.1f)
+            {
+                Material new_material = Instantiate(WorldMaterials[0]);
+
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material = new_material;
+                WorldChunks[_i].transform.GetChild(0).GetComponent<MeshRenderer>().material.SetFloat("Rotation", -45.0f);
+            }
         }
     }
 
@@ -82,7 +227,7 @@ public class WorldChunker : MonoBehaviour
             float Xpos = WorldChunks[0].transform.position.x;
             float Zpos = WorldChunks[0].transform.position.z + (WorldChunks[_i].transform.localScale.z * ChunkZSize);
 
-            WorldChunks[_i].transform.position = new Vector3(Xpos, 0, Zpos); 
+            WorldChunks[_i].transform.position = new Vector3(Xpos, 0, Zpos);
         }
 
         if (_i == 3)
